6b2f0ae9936d8b4ec8a584863bda94c94890200f
Лидия Дворкина <lideya832@mail.ru>
2022-10-26 22:03:25 +0300

    Домашнее задание: курс 2, урок 1, задача про машины

--- src/Car.java
+++ src/Car.java
@@ -1,13 +1,19 @@
 public class Car {
-    String brend;
-    String model;
-    double engineVolume;
-    String color;
-    int productionYear;
-    String productionCountry;
+    public String brend;
+    public String model;
+    public double engineVolume;
+    public String color;
+    public int productionYear;
+    public String productionCountry;
 
-    Car (String brend){
-        this.brend = brend;
+    public Car (){
+        this.brend = "default";
+        this.model = "default";
+        this.engineVolume = 1.5;
+        this.color = "белый";
+        this.productionYear = 2000;
+        this.productionCountry = "default";
+
            }
            void avto () {
                System.out.println(brend + " " + model + "," + productionYear + " год выпуска, сборка в " + productionCountry
--- src/Main.java
+++ src/Main.java
@@ -1,49 +1,48 @@
 public class Main {
     public static void main(String[] args) {
 
-        Car lada = new Car("Lada Granta");
-        lada.brend = "Lada";
+        Car lada = new Car ();
+
         lada.model = "Granda";
         lada.engineVolume = 1.7;
-        lada.color = "желтого";
+        lada.color = "желтый";
         lada.productionYear = 2015;
         lada.productionCountry = "России";
         lada.avto();
 
-        Car audi = new Car("Audi A8 50 L TDI quattro");
+        Car audi = new Car();
         audi.brend = "Audi";
-        audi.model = "A8 50 L TDI quattro";
+
         audi.engineVolume = 3.0;
         audi.color = "черный";
         audi.productionYear = 2020;
         audi.productionCountry = "Германии";
         audi.avto();
 
-        Car bmw = new Car("BMW Z8");
+        Car bmw = new Car();
         bmw.brend = "BMW";
         bmw.model = "Z8";
-        bmw.engineVolume = 3.0;
+
         bmw.color = "черный";
         bmw.productionYear = 2021;
         bmw.productionCountry = "Германии";
-        bmw.avto();
+             bmw.avto();
 
-        Car kia = new Car("Kia Sportage");
+        Car kia = new Car();
         kia.brend = "Kia";
-        kia.model = "Sportage 4-го поколения";
+        kia.model = "portaSge 4-го поколения";
         kia.engineVolume = 2.4;
-        kia.color = "красный";
+
         kia.productionYear = 2018;
         kia.productionCountry = "Южной Корее";
         kia.avto();
 
-        Car hyundai = new Car("Hyundai Avante");
+        Car hyundai = new Car();
         hyundai.brend = "Hyundai";
         hyundai.model = "Avante";
         hyundai.engineVolume = 1.6;
         hyundai.color = "оранжевый";
-        hyundai.productionYear = 2016;
-        hyundai.productionCountry = "Южной Корее";
+
         hyundai.avto();
 
 
